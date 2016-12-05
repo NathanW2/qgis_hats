d1dc2b3c7cf9e0bbffd3b81379fa52d382b37e50
Nathan Woodrow <madmanwoo@gmail.com>
2016-12-05 22:57:04 +1000

    Hats hats hats

---
+++ README.md
@@ -0,0 +1,12 @@
+# Hats for QGIS icons
+
+Because
+
+Exec #1: Item six on the agenda: "The Meaning of Life" Now uh, Harry, you've had some thoughts on this.
+Exec #2: Yeah, I've had a team working on this over the past few weeks, and what we've come up with can be reduced to two fundamental concepts. One: People aren't wearing enough hats. Two: Matter is energy. In the universe there are many energy fields which we cannot normally perceive. Some energies have a spiritual source which act upon a person's soul. However, this "soul" does not exist ab initio as orthodox Christianity teaches; it has to be brought into existence by a process of guided self-observation. However, this is rarely achieved owing to man's unique ability to be distracted from spiritual matters by everyday trivia.
+Exec #3: What was that about hats again?
+Exec #2: Oh, Uh... people aren't wearing enough.
+Exec #1: Is this true?
+Exec #4: Certainly. Hat sales have increased but not pari passu, as our research...
+Exec #3: [Interrupting] "Not wearing enough"? enough for what purpose?
+Exec #5: Can I just ask, with reference to your second point, when you say souls don't develop because people become distracted...
---
+++ __init__.py
@@ -0,0 +1,95 @@
+#-----------------------------------------------------------
+# Copyright (C) 2015 Martin Dobias
+#-----------------------------------------------------------
+# Licensed under the terms of GNU GPL 2
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#---------------------------------------------------------------------
+
+import os
+import urllib2
+from urllib.error import HTTPError
+
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
+def classFactory(iface):
+    return HatsSoManyHats(iface)
+
+def resolve(name):
+    filename = __file__
+    f = os.path.join(os.path.dirname(filename), name)
+    return f
+
+HATSDIR = resolve("SoManyHats")
+URL =  "https://github.com/qgis/QGIS/raw/master/images/icons/{icon}"
+
+
+def not_wearing_enough(month, day):
+    day = str(day).zfill(2)
+    month = str(month).zfill(2)
+    fullpath = resolve(HATSDIR + "\{0}-{1}.png".format(str(month), str(day)))
+    monthonly = resolve(HATSDIR + "\{0}.png".format(str(month)))
+    if os.path.exists(fullpath):
+        return fullpath
+    elif os.path.exists(monthonly):
+        return monthonly
+    return None
+
+
+def get_more_hats(month, day):
+    if not os.path.exists(HATSDIR):
+        os.makedirs(HATSDIR)
+    day = str(day).zfill(2)
+    month = str(month).zfill(2)
+    fullname = "{0}-{1}.png".format(str(month), str(day))
+    monthonlyname = "{0}.png".format(str(month))
+    fullpath = resolve(HATSDIR + "\\" + fullname)
+    monthonly = resolve(HATSDIR + "\\" + monthonlyname)
+    data = None
+    try:
+        fullname = "qgis-icon.png"
+        url = URL.format(icon=fullname)
+        response = urllib2.urlopen(url)
+        data = response.read()
+        with open(fullpath, "wb") as f:
+            f.write(data)
+        return fullpath
+    except Exception as ex:
+        pass
+
+    try:
+        monthonlyname = "qbrowser-icon.png"
+        url = URL.format(icon=monthonlyname)
+        print url
+        response = urllib2.urlopen(url)
+        data = response.read()
+        with open(monthonly, "wb") as f:
+            f.write(data)
+        return monthonly
+    except Exception as ex:
+        pass
+
+
+class HatsSoManyHats:
+    def __init__(self, iface):
+        self.iface = iface
+
+    def initGui(self):
+        current = QDateTime.currentDateTime()
+        month = current.date().month()
+        day = current.date().day()
+        hat = not_wearing_enough(month, day)
+        if not hat:
+            hat = get_more_hats(month, day)
+
+        if hat:
+            icon = QIcon(hat)
+            QApplication.instance().setWindowIcon(icon)
+            self.iface.mainWindow().setWindowIcon(icon)
+
+    def unload(self):
+        pass
