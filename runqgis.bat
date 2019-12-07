1442e72636041450c571f5544aedc5a7f2820daa
Nathan Woodrow <woodrow.nathan@gmail.com>
2019-12-07 20:50:00 +1000

    Update to run on QGIS 3.10

--- runqgis.bat
+++ runqgis.bat
@@ -1,2 +1,2 @@
 SET QGIS_PLUGINPATH=%CD%\src
-"C:\Program Files\QGIS 3.4\bin\qgis-bin.exe" --profiles-path %CD%\qgis --profile plugin
+"C:\Program Files\QGIS 3.10\bin\qgis-bin.exe" --profiles-path %CD%\qgis --profile plugin
